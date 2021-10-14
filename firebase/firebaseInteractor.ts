8057ae854ce23b8cddfa4aeb0c613631324cf61e
Octillerysnacker <octillerysnacker@gmail.com>
2021-10-13 23:06:16 -0400

    Copied stuff FirebaseInteractor and models/types from vocab buddy

--- firebase/firebaseInteractor.ts
+++ firebase/firebaseInteractor.ts
@@ -1,5 +1,7 @@
 import * as firebase from "firebase/app";
-import "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, sendEmailVerification, signInWithEmailAndPassword } from "firebase/auth";
+import { getFirestore, collection, doc, setDoc, getDoc, DocumentData } from "firebase/firestore";
+import { User } from "../models/types";
 
 
 // Your web app's Firebase configuration
@@ -15,6 +17,68 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
 
+/**
+ * A class to interact with firebase. This class stores the current state,
+ * including a reference to the firestore, and the current authenticated user.
+ * This adds a level of abstraction around firebase, so that this is the only object dealing with the server.
+ * Stolen from vocab buddy.
+ */
 export default class FirebaseInteractor {
-    auth = firebase.auth();
+    auth = getAuth(app);
+    db = getFirestore();
+    currentUser: User | null = null;
+    /**
+    * Creates an account for a user
+    */
+    async createAccount(
+        email: string,
+        password: string,
+    ) {
+        let userAuth = await createUserWithEmailAndPassword(
+            this.auth,
+            email,
+            password
+        );
+        if (userAuth.user?.uid == null) {
+            throw new Error("No actual user");
+        }
+        sendEmailVerification(userAuth.user);
+    }
+
+    async signInWithUsernameAndPassword(username: string, password: string) {
+        // idk what this do - robert
+        // this.unsubscribe?.apply(this);
+
+        await signInWithEmailAndPassword(this.auth, username, password);
+        await this.createCurrentUser();
+    }
+
+    async createCurrentUser() {
+        try {
+            this.currentUser = await this.getUser(undefined);
+        } catch (error) {
+            // Hopefully this does not happen.
+            console.log(error);
+        }
+    }
+
+    async getUser(id: string | undefined): Promise<User> {
+        let idToUse = id || this.auth.currentUser?.uid;
+        let user = await getDoc(doc(collection(this.db, "users"), idToUse));
+
+        let userData = user.data();
+        if (idToUse != null && userData != null) {
+            return this.getUserFromData(idToUse, userData);
+        } else {
+            throw new Error("Invalid user");
+        }
+    }
+    // given a user document, returns a user object
+    getUserFromData(id: string, userData: DocumentData): User {
+        return {
+            id: id,
+            sessionId: userData.sessionId === undefined ? -1 : userData.sessionId,
+        };
+    }
+
 }
