cbc4e8d42cbfd9e83f310f67d928a69f83113dd1
ofloody <oliviafloody@gmail.com>
2021-10-17 15:45:43 -0400

    test onboarding screen input

--- components/OnboardingScreens/OnboardingScreens.tsx
+++ components/OnboardingScreens/OnboardingScreens.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import OnboardingScreen from './OnboardingScreen';
 
 export default function OnboardingScreens() {
-    return 0;
+    return (<OnboardingScreen screenContent="hey there" screenHeader="Hey" screenImage="/"></OnboardingScreen>);
+
 }
