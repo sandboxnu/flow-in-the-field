e706ee86dadb5d75017517433a214af363c5ea4a
Jack Rosen <jrosen081@gmail.com>
2021-10-18 12:35:45 -0400

    Added signing in/creating account/resetting password (#5)

---
+++ firebase/firebaseInteractor.ts
@@ -0,0 +1,54 @@
+import * as firebase from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth, sendEmailVerification, sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC9cAIskIjacQRxhbJlJxJNizGOlbRj4gk",
+    authDomain: "flowinthefield.firebaseapp.com",
+    projectId: "flowinthefield",
+    storageBucket: "flowinthefield.appspot.com",
+    messagingSenderId: "788116866647",
+    appId: "1:788116866647:web:0e2cbfc671576c8089c512"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+
+/**
+ * A class to interact with firebase. This class stores the current state,
+ * including a reference to the firestore, and the current authenticated user.
+ * This adds a level of abstraction around firebase, so that this is the only object dealing with the server.
+ * Stolen from vocab buddy.
+ */
+export default class FirebaseInteractor {
+    auth = getAuth(app);
+    db = getFirestore();
+
+    /**
+    * Creates an account for a user, but does not store them in the db yet, since we don't know what we will store
+    */
+    async createAccount(
+        email: string,
+        password: string,
+    ) {
+        let userAuth = await createUserWithEmailAndPassword(
+            this.auth,
+            email,
+            password
+        );
+        if (userAuth.user?.uid == null) {
+            throw new Error("No actual user");
+        }
+        sendEmailVerification(userAuth.user);
+    }
+
+    async signInWithUsernameAndPassword(username: string, password: string) {
+        await signInWithEmailAndPassword(this.auth, username, password);
+    }
+
+    async resetPassword(email: string) {
+        await sendPasswordResetEmail(this.auth, email)
+    }
+}
