7a1b2b5d71bfe8cde7bd52b28dfecf8d10fdfa9e
koulopoulos <00307645@student.necc.edu>
2021-11-04 22:00:39 -0400

    Removed FinalOnboardingScreen, Cleaned code, Fixed font styling

--- src/screens/Onboarding/OnboardingScreen.tsx
+++ src/screens/Onboarding/OnboardingScreen.tsx
@@ -1,34 +1,44 @@
 import React, { ReactElement } from 'react';
-import { Image, StyleSheet, View, Text, TouchableHighlight } from 'react-native';
+import { Image, StyleSheet, View, Text, Pressable } from 'react-native';
 
+const FLOW_ICON = '../../assets/flow-icon-light.png'
 
 interface OnboardingScreenProps {
     screenContent: string;
     bgColor: string;
+    hasNavButton?: boolean;
+    navButtonTitle?: string;
+    navigation?: any;
+    route?: string;
 }
 
 export default function OnboardingScreen({
     screenContent, 
-    bgColor}: OnboardingScreenProps): ReactElement {
+    bgColor,
+    hasNavButton,
+    navButtonTitle,
+    navigation,
+    route
+}: OnboardingScreenProps): ReactElement {
 
-    const backgroundStyle = {
-        backgroundColor: bgColor
+    const backgroundStyle = { 
+        backgroundColor: bgColor 
     };
 
     return (
-        <TouchableHighlight style={styles.touchable}>
-            <View style={[styles.container, backgroundStyle]}>
-                <Image style={styles.image} source={require('../../assets/flow-icon-light.png')}/>
-                <Text style={styles.content}> {screenContent} </Text>
-            </View>
-        </TouchableHighlight>
+        <View style={[styles.container, backgroundStyle]}>
+            <Image style={styles.image} source={require(FLOW_ICON)}/>
+            <Text style={styles.content}>{ screenContent }</Text>
+            {hasNavButton &&
+                <Pressable onPress={() => {navigation.navigate(route)}} style={styles.button}>
+                    <Text style={styles.buttonContent}>{ navButtonTitle }</Text>
+                </Pressable>
+            }
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
-    touchable: {
-        height: '100%'
-    },
     container: {
         flex: 1,
         alignItems: 'center',
@@ -44,9 +54,27 @@ const styles = StyleSheet.create({
         marginBottom: 24
     },
     content: {
-        fontFamily: 'Futura',
-        fontSize: 20,
+        fontFamily: 'Avenir Next',
+        fontWeight: '500',
+        fontSize: 18,
         color: '#FFF',
         textAlign: 'center'
+    },
+    button: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#FFF',
+        width: 130,
+        maxHeight: 30,
+        marginTop: 32,
+        borderRadius: 8,
+        overflow: 'hidden'
+    },
+    buttonContent: {
+        backgroundColor: '#FFF',
+        fontFamily: 'Futura',
+        fontSize: 20,
+        color: '#D16B50'
     }
 });
--- src/screens/Onboarding/OnboardingScreens.tsx
+++ src/screens/Onboarding/OnboardingScreens.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import LoginPage from '../Login/login';
-import SignInFlow from '../Login/signInFlow';
+import { StyleSheet } from 'react-native';
 import OnboardingScreen from './OnboardingScreen';
-import FinalOnboardingScreen from './FinalOnboardingScreen';
 import { TURQUOISE, BLUE, PURPLE } from '../../constants/colors';
 import Swiper from 'react-native-swiper';
 
@@ -11,21 +8,22 @@ interface OnboardingScreenProps {
     navigation: any;
 }
 
-export default function OnboardingScreens({ navigation }: OnboardingScreenProps) {
+export default function OnboardingScreens({ 
+    navigation 
+}: OnboardingScreenProps) {
     return (
         <Swiper 
             showsButtons={false} 
             loop={false} 
             pagingEnabled={true}
-            dotStyle={{backgroundColor:'rgba(255,255,255,0.5)', width: 16, height: 16, borderRadius: 16}}
-            activeDotStyle={{backgroundColor:'#FFF', width: 16, height: 16, borderRadius: 16}}>
+            dotStyle={styles.dots}
+            activeDotStyle={styles.activeDots}>
             <OnboardingScreen 
                 bgColor={BLUE}
                 screenContent={
                     "Thank you for agreeing to \nparticipate in our study! " +
                     "For the next week, your goal is to learn as many " +
-                    "Turkish words as possible."
-                }/>
+                    "Turkish words as possible."}/>
             <OnboardingScreen 
                 bgColor={TURQUOISE}
                 screenContent={
@@ -34,17 +32,33 @@ export default function OnboardingScreens({ navigation }: OnboardingScreenProps)
                     "words and a set of Turkish words.\n • You'll try to " +
                     "match each English word to the correct Turkish word, " +
                     "then you'll receive feedback.\n • The more you practice, " +
-                    "the better you'll get!"
-                }/>
-            <FinalOnboardingScreen 
+                    "the better you'll get!"}/>
+            <OnboardingScreen 
                 bgColor={PURPLE}
-                navigation={navigation}
                 screenContent={
                     "Over the next week, you can use this app to practice " +
                     "as much or as little as you like. At the end of the week, " +
                     "you'll receive a vocabulary test. The better you do on the " +
-                    "test, the more money you'll win!"
-                }/>
+                    "test, the more money you'll win!"}
+                hasNavButton={true}
+                navButtonTitle={"register"}
+                navigation={navigation}
+                route={"SignInFlow"}/>
         </Swiper>
     )
 }
+
+const styles = StyleSheet.create({
+    dots: {
+        backgroundColor:'rgba(255,255,255,0.5)', 
+        width: 16, 
+        height: 16, 
+        borderRadius: 16
+    },
+    activeDots: {
+        backgroundColor:'#FFF', 
+        width: 16, 
+        height: 16, 
+        borderRadius: 16
+    }
+});
