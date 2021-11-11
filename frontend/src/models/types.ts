9235da796afcf3dae1fe9184fcdf7735e4dcf84f
Jack Rosen <jrosen081@gmail.com>
2021-11-10 20:12:36 -0500

    Created Home Screen (#53)

--- frontend/src/models/types.ts
+++ frontend/src/models/types.ts
@@ -1,1 +1,8 @@
 export type NumPairs = 2 | 4 | 8;
+export type GameType = "matching" | "multipleChoice";
+export interface User {
+    email: string;
+    testDate: Date;
+    numPairs: NumPairs;
+    gameType: GameType;
+}
