77c9b6585c63aa44d003503adbd2067fd7d120a4
koulopoulos <00307645@student.necc.edu>
2021-11-14 14:22:24 -0500

    Operational upload script (words uploaded)

--- uploadscript/script.js
+++ uploadscript/script.js
@@ -1,4 +1,19 @@
-const fs = require('fs')
-const firebase = require('firebase-admin');
+require('dotenv').config();
+const admin = require('firebase-admin');
+const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS)
 
-console.log("script")
+const WORDS = require('./words/words.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+let db = admin.firestore();
+
+async function uploadWords() {
+    for (let word of WORDS.words) {
+        const res = await db.collection('words').add(word);
+    }
+}
+
+uploadWords().then(() => console.log("Uploaded successfully.")).catch(console.log)
