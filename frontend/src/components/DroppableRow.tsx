d884df5f3de11079775ffd3399bf878ad5930cb9
Octillerysnacker <octillerysnacker@gmail.com>
2021-11-21 16:11:41 -0500

    styles the drax views better

--- frontend/src/components/DroppableRow.tsx
+++ frontend/src/components/DroppableRow.tsx
@@ -1,5 +1,4 @@
-import { TouchableOpacity } from "react-native-gesture-handler";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React from "react";
 import { DraxView } from "react-native-drax";
 interface DroppableRowProps {
@@ -7,25 +6,62 @@ interface DroppableRowProps {
     english?: string,
     removeWord: () => void;
     wordDropped: (word: string) => void;
+    // submitted: boolean;
+    // correctEnglishWord: string
 }
 
 export default function DroppableRow({ turkish, english, removeWord, wordDropped }: DroppableRowProps) {
     return (<View style={styles.container}>
-        {english ? <TouchableOpacity onPress={removeWord}><Text>{english}</Text></TouchableOpacity> : <DraxView style={styles.draxView}
-            onReceiveDragDrop={({ dragged: { payload } }: { dragged: { payload: string } }) => {
-                wordDropped(payload);
-            }}></DraxView>}
-        <Text>{turkish}</Text>
+        {english ?
+            <TouchableOpacity style={styles.englishTextContainer} onPress={removeWord}>
+                <Text style={styles.englishText}>{english}</Text>
+            </TouchableOpacity>
+            : <DraxView style={styles.draxView}
+                onReceiveDragDrop={({ dragged: { payload } }: { dragged: { payload: string } }) => {
+                    wordDropped(payload);
+                }}></DraxView>}
+        <Text style={styles.turkishText}>{turkish}</Text>
     </View>)
 }
 
 const styles = StyleSheet.create({
     draxView: {
+        borderColor: "#5BBAB7",
+        borderWidth: 3,
         width: "40%",
-        borderColor: "green",
-        borderWidth: 5
+        borderStyle: "dashed",
+        height: "100%",
+        marginHorizontal: "5%",
+        borderRadius: 0.0001
+    },
+    turkishText: {
+        width: "40%",
+        textAlign: "center",
+        backgroundColor: "#C4C4C4",
+        paddingVertical: "2%",
+        color: "white"
+    },
+    englishTextContainer: {
+        width: "40%",
+        textAlign: "center",
+        borderColor: "white",
+        borderStyle: "dashed",
+        borderWidth: 4,
+        backgroundColor: "#5EAFDF",
+        paddingVertical: "2%",
+        color: "white",
+        marginHorizontal: "5%",
+        borderRadius: 0.000001
+    },
+    englishText: {
+        color: 'white',
+        textAlign: 'center'
     },
     container: {
-        flexDirection: "row"
+        flexDirection: "row",
+        alignItems: 'center',
+        width: "100%",
+        height: 30,
+        marginVertical: "1%"
     }
 });
--- frontend/src/screens/pairingGameScreen.tsx
+++ frontend/src/screens/pairingGameScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { BLUE, GREY } from "../constants/colors";
 import FirebaseInteractor from "../firebase/firebaseInteractor";
 import { User, Word } from "../models/types";
@@ -11,35 +11,39 @@ const fi = new FirebaseInteractor();
 interface MaybeWordPair {
     english?: string;
     turkish: string;
+    correctEnglishWord: string;
 }
 
 export default function PairingGameScreen() {
     const [englishWords, setEnglishWords] = useState<string[]>()
     const [turkishWords, setTurkishWords] = useState<MaybeWordPair[]>()
     const [user, setUser] = useState<User>();
+    const [submitted, setSubmitted] = useState(false);
 
     useEffect(() => {
         fi.getUser().then(user => {
             setUser(user);
             fi.getXRandomPairs(user.numPairs).then(words => {
                 setEnglishWords(durstenfeldShuffle(words.map((word, i) => word.english)));
-                setTurkishWords(durstenfeldShuffle(words.map((word, i) => ({ turkish: word.turkish }))));
+                setTurkishWords(durstenfeldShuffle(words.map((word, i) => ({ turkish: word.turkish, correctEnglishWord: word.english }))));
             }).catch(console.error);
         }).catch(console.error);
     }, []);
 
-    if (englishWords?.length === 0) {
+    if (englishWords === undefined) {
         return <Text>loading</Text>
     }
 
+    const correctValues = turkishWords?.filter(({ english, correctEnglishWord }) => english === correctEnglishWord).length ?? 0;
     return (
         <DraxProvider>
             <View style={styles.container}>
                 <View style={styles.column}>
-                    {englishWords?.map(word =>
-                        <DraxView payload={word} key={word} style={styles.draxView}>
-                            <Text style={styles.english}>{word}</Text>
-                        </DraxView>)}
+                    {submitted ? <Text style={styles.scoreText}>{correctValues}/{turkishWords?.length ?? 0}</Text> :
+                        englishWords?.map(word =>
+                            <DraxView payload={word} key={word} style={styles.draxView}>
+                                <Text style={styles.english}>{word}</Text>
+                            </DraxView>)}
                 </View>
                 <View style={styles.turkishContainer}>
                     {turkishWords?.map((word, i) => (
@@ -47,7 +51,7 @@ export default function PairingGameScreen() {
                             key={word.turkish}
                             wordDropped={(newWord) => {
                                 const newTurkishWords = turkishWords
-                                newTurkishWords[i] = { english: newWord, turkish: word.turkish }
+                                newTurkishWords[i] = { english: newWord, turkish: word.turkish, correctEnglishWord: word.correctEnglishWord }
                                 setTurkishWords(newTurkishWords)
                                 setEnglishWords(englishWords?.filter((word) => word !== newWord))
                             }}
@@ -56,7 +60,7 @@ export default function PairingGameScreen() {
                             removeWord={
                                 () => {
                                     const newTurkishWords = [...turkishWords]
-                                    newTurkishWords[i] = { turkish: word.turkish }
+                                    newTurkishWords[i] = { turkish: word.turkish, correctEnglishWord: word.correctEnglishWord }
                                     const newEnglishWords = [...englishWords!]
                                     newEnglishWords?.push(word.english!)
                                     console.log(newEnglishWords)
@@ -66,6 +70,7 @@ export default function PairingGameScreen() {
                         />))}
                 </View>
             </View>
+            <TouchableOpacity style={styles.doneButton} onPress={() => setSubmitted(true)}><Text style={styles.doneButtonTitle}>done</Text></TouchableOpacity>
         </DraxProvider>
     )
 }
@@ -83,36 +88,51 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: "center",
         width: "100%",
-        height: "100%"
+        height: "100%",
     },
     column: {
         flex: 1,
         width: "100%",
         alignItems: "center",
         paddingTop: "10%",
-        height: "50%",
         flexWrap: "wrap",
         flexDirection: "row"
 
     },
     turkishContainer: {
-
+        width: "100%",
+        flex: 1
     },
     english: {
         ...defaultStyle.default,
-        paddingVertical: "7%",
+        paddingVertical: "10%",
+        marginVertical: "4%",
         backgroundColor: BLUE
-    },
-    turkish: {
-        ...defaultStyle.default,
-        backgroundColor: GREY
     },
     draxView: {
         width: "40%",
         marginHorizontal: "5%",
-        height: "15%"
+        height: "15%",
+        marginVertical: "3%"
     },
-    turkishDraxView: {
+    doneButton: {
+        backgroundColor: "#D16B50",
+        borderRadius: 12,
+        height: "5%",
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: "20%",
+        marginBottom: "2%",
+        marginLeft: "40%"
+    },
+    doneButtonTitle: {
+        color: "white"
+    },
+    scoreText: {
+        color: BLUE,
+        fontSize: 124,
+        textAlign: "center",
+        width: "100%"
     }
 })
 
