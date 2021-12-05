ce5d1b8556c309f7f7a722d62164aac0a629f430
koulopoulos <00307645@student.necc.edu>
2021-12-05 15:28:29 -0500

    types

--- frontend/src/models/types.ts
+++ frontend/src/models/types.ts
@@ -1,5 +1,9 @@
 export type NumPairs = 2 | 4 | 8;
+
 export type GameType = "pairing" | "selecting";
+
+export type UID = string;
+
 export interface User {
     email: string;
     testDate: Date;
@@ -8,6 +12,26 @@ export interface User {
 }
 
 export interface Word {
-    turkish: string
-    english: string
+    turkish: string;
+    english: string;
+}
+
+export interface Session {
+    user: UID;
+    startTime: Date;
+    endTime: Date;
+}
+
+export interface Round {
+    session: UID;
+    startTime: Date;
+    endTime: Date;
+    words: UID[];
+}
+
+export interface Match {
+    round: UID;
+    turkish: string;
+    english: string;
+    correct: boolean;
 }
