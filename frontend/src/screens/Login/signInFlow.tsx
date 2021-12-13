4d9fdec8671de1b6685b148bac09bcceffaf8624
Octillerysnacker <octillerysnacker@gmail.com>
2021-12-13 15:23:32 -0500

    added code

--- frontend/src/screens/Login/signInFlow.tsx
+++ frontend/src/screens/Login/signInFlow.tsx
@@ -1,5 +1,6 @@
 import { useNavigation } from "@react-navigation/core"
 import React, { useState } from "react"
+import FirebaseInteractor from "../../firebase/firebaseInteractor"
 import AccountSettings from "./accountSettings"
 import LoginPage from "./login"
 import RecoverPasswordPage from "./recoverPassword"
@@ -7,18 +8,27 @@ import SignUpPage from "./signUp"
 
 
 type CurrentPage = "signUp" | "signIn" | "recoverPassword"
-
+const fi = new FirebaseInteractor();
 export default function SignInFlow() {
+
+    const userSignedIn = () => {
+        if (fi.auth.currentUser?.emailVerified) {
+            navigation.navigate("HomeScreen")
+        } else {
+            navigation.navigate("EmailVerification")
+        }
+    };
+
     const [currentPage, setCurrentPage] = useState<CurrentPage>("signUp")
     const navigation = useNavigation();
     if (currentPage == 'signIn') {
         return (<LoginPage
             goToSignUp={() => setCurrentPage("signUp")}
             goToRecoveryScreen={() => setCurrentPage("recoverPassword")}
-            goToAccountSettings={() => navigation.navigate("HomeScreen")} />)
+            goToAccountSettings={userSignedIn} />)
     } else if (currentPage == "recoverPassword") {
         return (<RecoverPasswordPage goToSignIn={() => setCurrentPage("signIn")} />)
     } else {
-        return (<SignUpPage goToSignIn={() => setCurrentPage("signIn")} goToAccountSettings={() => navigation.navigate("HomeScreen")} />)
+        return (<SignUpPage goToSignIn={() => setCurrentPage("signIn")} goToAccountSettings={userSignedIn} />)
     }
 }
