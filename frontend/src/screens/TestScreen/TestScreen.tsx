cbd2964b148cc725fbc2dfb15e313db0055f4f3a
Shashank Jarmale <shashankjarmale1@gmail.com>
2022-04-03 17:10:58 -0400

    Added words for participant test screen

--- frontend/src/screens/TestScreen/TestScreen.tsx
+++ frontend/src/screens/TestScreen/TestScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Pressable } from "react-native";
 import { BLUE, GREY } from "../../constants/colors";
 import FirebaseInteractor from "../../firebase/firebaseInteractor";
 import { User, Word, UID, GameType } from "../../models/types";
@@ -9,7 +9,7 @@ import DroppableRow from "../../components/DroppableRow";
 import { useNavigation } from "@react-navigation/core";
 import { LoadingScreen } from "../../components/LoadingScreen";
 import { NavigationContainer } from "@react-navigation/native";
-import { ProgressBar, Colors } from 'react-native-paper';
+import AnimatedBar from "react-native-animated-bar";
 
 const fi = new FirebaseInteractor();
 
@@ -36,18 +36,34 @@ export interface TestScreenProps {
 export default function TestScreen(props: TestScreenProps) {
 
     const navigation = useNavigation();
-    const [page, setPage] = useState<number>(0);
+    const [page, setPage] = useState<number>(1);
     const numQuestions = 50;
+    const TESTWORDS = require('./testWords.json');
+    const [answer, setAnswer] = useState<string>("");
+    const[answers, setAnswers] = useState<string[]>([]);
+    const answerKey = ['no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'yes', 'yes', 'no', 'yes', 'no', 'yes', 'no', 'yes', 'yes', 'yes', 'no', 'yes', 'yes', 'yes', 'no', 'no', 'yes', 'yes', 'yes', 'no', 'yes', 'no', 'yes', 'no', 'no', 'yes', 'no', 'yes', 'yes', 'no', 'yes', 'no', 'yes', 'no', 'yes', 'no', 'yes', 'no', 'yes', 'yes', 'yes', 'yes', 'yes'];
+
+    const gradeAnswers = () => {
+        let correctAnswers = 0;
+        for (let question = 1; question <= numQuestions; question++) {
+            if (answers[question - 1] === answerKey[question - 1]) {
+                correctAnswers++;
+            }
+        }
+        return correctAnswers;
+    }
 
     React.useLayoutEffect(() => {
         navigation.setOptions(TEST_HEADER_OPTIONS);
       }, [navigation]);
 
     const testButtonOnPress = () => {
+        setAnswers([...answers, answer])
         if (page < numQuestions) {
+            setAnswer("");
             setPage(page + 1);
         } else {
-            navigation.navigate("TestResultsScreen", { correct: 30, total: 50 })
+            navigation.navigate("TestResultsScreen", { correct: gradeAnswers(), total: 50 })
         }
     }
 
@@ -55,15 +71,37 @@ export default function TestScreen(props: TestScreenProps) {
     return (
         <DraxProvider>
             <View style={styles.container}>
-                <Text style={{color: "#000", fontSize: 22}}>top</Text>
-                <View style={styles.progressContainer}>
-                    <ProgressBar style={styles.progressBar} progress={page / numQuestions} color={"#5eafdf"} />
-                    <Text style={{color: "#5eafdf", fontSize: 16}}>{page}/{numQuestions}</Text>
+                <View style={styles.row}>
+                    <AnimatedBar
+                        progress={page / numQuestions}
+                        height={6}
+                        borderColor="#FFF"
+                        barColor="#5EAFDF"
+                        fillColor="#5EAFDF20"
+                        fillStyle={{borderRadius: 10}}
+                        barStyle={{borderRadius: 10}}
+                        animate={false}
+                        duration={500}
+                        row
+                    >
+                    </AnimatedBar>
+                    <Text style={{color: "#5eafdf", fontSize: 16, paddingLeft: 8}}>{page} / {numQuestions}</Text>
                 </View>
-                
-                <Text style={{color: "#000", fontSize: 22}}>bottom</Text>
-                <TouchableOpacity style={styles.startButton} onPress={testButtonOnPress}>
-                    <Text style={{color: "#FFF", fontSize: 22}}>continue</Text>
+                <Text style={{fontSize: 36}}>Do these words share the same meaning?</Text>
+                <View style={styles.wordsContainer}>
+                    <Text style={styles.wordsText}>{TESTWORDS.testWords[page - 1]["turkish"]}</Text>
+                    <Text style={styles.wordsText}>{TESTWORDS.testWords[page - 1]["english"]}</Text>
+                </View>
+                <View style={styles.answerContainer}>
+                    <TouchableOpacity onPress={() => setAnswer("yes")} style={answer === "yes" ? styles.selectedAnswerButton : styles.unselectedAnswerButton}>
+                        <Text style={answer === "yes" ? styles.selectedAnswerText : styles.unselectedAnswerText}>Yes</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => setAnswer("no")} style={answer === "no" ? styles.selectedAnswerButton : styles.unselectedAnswerButton}>
+                        <Text style={answer === "no" ? styles.selectedAnswerText : styles.unselectedAnswerText}>No</Text>
+                    </TouchableOpacity>
+                </View>
+                <TouchableOpacity disabled={!answer} onPress={testButtonOnPress} style={answer ? styles.continueButton : {...styles.continueButton, opacity: 0.5}}>
+                    <Text style={{color: "#6E81E7", fontSize: 22}}>continue ></Text>
                 </TouchableOpacity>
             </View>
         </DraxProvider>
@@ -77,38 +115,76 @@ const defaultStyle = StyleSheet.create({
 
 const styles = StyleSheet.create({
    container: {
-    flexDirection: "column",
-    justifyContent: 'space-between',
-    backgroundColor: '#FFF',
-    alignItems: "center",
-    width: "100%",
-    height: "100%",
-    paddingTop: "5%"
+        flexDirection: "column",
+        justifyContent: 'space-between',
+        backgroundColor: '#FFF',
+        alignItems: "center",
+        width: "100%",
+        height: "100%",
+        paddingTop: "5%",
+        paddingLeft: "5%",
+        paddingRight: "5%",
    },
-   progressContainer: {
-    flexDirection: "row",
-    justifyContent: 'center',
-    backgroundColor: '#FFF',
-    alignItems: "center",
-    width: "100%",
-    paddingTop: "5%"
-   },
-   startButton: {
-    backgroundColor: "#5FBFF8",
-    borderColor: "#5FBFF8",
-    borderWidth: 2,
-    borderRadius: 12,
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '7.5%',
-    width: '75%',
-    paddingHorizontal: "5%",
-    marginHorizontal: '3%',
-    marginVertical: "1%",
-    paddingVertical: 10,
-    marginTop: "10%"
+   rowText: {
+        marginRight: 20,
+  },
+  row: {
+        flexDirection: "row",
+        alignItems: "center",
+  },
+    wordsContainer: {
+        width: "100%",
+        flexDirection: "row",
+        justifyContent: "space-around",
     },
-    progressBar: {
-        width: 200,
+    wordsText: {
+        fontSize: 40,
+        color: "#5EAFDF",
+    },
+    answerContainer: {
+        width: "100%",
+        height: "20%",
+        flexDirection: "column",
+        justifyContent: "space-around",
+        alignItems: "center",
+    },
+    selectedAnswerButton: {
+        backgroundColor: "#6E81E7",
+        borderColor: "#6E81E7",
+        borderWidth: 3,
+        borderRadius: 12,
+        justifyContent: 'center',
+        height: '40%',
+        width: '90%',
+        paddingLeft: "10%",
+    },
+    unselectedAnswerButton: {
+        backgroundColor: "#FFF",
+        borderColor: "#6E81E7",
+        borderWidth: 3,
+        borderRadius: 12,
+        justifyContent: 'center',
+        height: '40%',
+        width: '90%',
+        paddingLeft: "10%",
+    },
+    selectedAnswerText: {
+        color: "#FFF", 
+        fontSize: 24,
+    },
+    unselectedAnswerText: {
+        color: "#6E81E7", 
+        fontSize: 24
+    },
+    continueButton: {
+        flex: 1,
+        alignItems: 'flex-end',
+        justifyContent: 'flex-start',
+        backgroundColor: "#FFF",
+        width: "100%",
+        maxHeight: "6%",
+        marginTop: "7%",
+        overflow: 'hidden',
+        marginBottom: "3%",
     }
 })
