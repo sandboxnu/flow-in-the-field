fe4f55f472213195136f9762747a2034a1dc6df5
Octillerysnacker <octillerysnacker@gmail.com>
2022-05-28 16:40:11 -0500

    added basic admin screen stuff

--- frontend/src/screens/adminScreen.tsx
+++ frontend/src/screens/adminScreen.tsx
@@ -20,13 +20,44 @@ export interface AdminScreenProps {
 
 export default function AdminScreen(props: AdminScreenProps) {
 
+
     return (
-        <DraxProvider>
-            <Text>This is admin placeholder hooray</Text>
+        <View>
+            <Text>Press the buttons below to export data as an email with a CSV attachment.</Text>
+
+            <Text>Practice Data</Text>
+
+            <Text>The Practice Data CSV will contain the following information for all practice rounds played by participants:</Text>
+            <Text>User ID</Text>
+            <Text>Game Type</Text>
+            <Text>Number of Turkish Words</Text>
+            <Text>Session ID</Text>
+            <Text>Round ID</Text>
+            <Text>Start Time</Text>
+            <Text>Duration</Text>
+            <Text>Score</Text>
+
             <PrimaryButton onPress={async () => {
                 await promptExportEmail(await generateRoundsCSV());
-            }} disabled={false} text="create csv" />
-        </DraxProvider>
+            }} disabled={false} text="Export Practice CSV" />
+
+            <Text>Round Data</Text>
+
+            <Text>The Test Data CSV will contain the following information for all test questions answered by participants:</Text>
+            <Text>Participant ID</Text>
+            <Text>Game Type</Text>
+            <Text>Test Session ID</Text>
+            <Text>Question Number</Text>
+            <Text>Start Time</Text>
+            <Text>End Time</Text>
+            <Text>Duration</Text>
+            <Text>Answer</Text>
+            <Text>Correctness</Text>
+
+            <PrimaryButton onPress={async () => {
+                await promptExportEmail(await generateTestRoundsCSV());
+            }} disabled={false} text="Export Test CSV" />
+        </View>
     )
 }
 
@@ -38,3 +69,7 @@ const defaultStyle = StyleSheet.create({
 const styles = StyleSheet.create({
 
 })
+function generateTestRoundsCSV(): Object[] | PromiseLike<Object[]> {
+    throw new Error("Function not implemented.");
+}
+
