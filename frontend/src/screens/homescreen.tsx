2cdb52513f7de148977da84497b1d9dee95d71ac
eco20 <eireanco@gmail.com>
2022-06-26 18:01:27 -0400

    make homescreen responsive

--- frontend/src/screens/homescreen.tsx
+++ frontend/src/screens/homescreen.tsx
@@ -74,7 +74,7 @@ export default function Homescreen(props: HomescreenProps) {
             <View style={styles.buttonBox}>
                 <TextIconButton onPress={() => startSession()} text="Start a new session" icon={require("../assets/start-session-icon.png")} />
                 <TextIconButton onPress={() => startTest()} text="Take the test" icon={require("../assets/flow-icon-test.png")} testNotAvailable={!testAvailable()} />
-                {user.role == Role.ADMIN ? <PrimaryButton onPress={() => navigation.navigate("AdminScreen")} text="Admin" disabled={false} /> : <View />}
+                {user.role == Role.ADMIN && <TextIconButton onPress={() => navigation.navigate("AdminScreen")} text="Admin" icon={require("../assets/admin-icon.png")}/>}
                 <TextIconButton onPress={() => navigation.navigate("SettingsScreen")} text="Settings" icon={require("../assets/settings-icon.png")} />
                 <TextIconButton onPress={() => navigation.navigate("RevisitOnboarding", { signedIn: true })} text="About" icon={require("../assets/about-icon.png")} />
             </View>
@@ -83,11 +83,13 @@ export default function Homescreen(props: HomescreenProps) {
 
 const styles = StyleSheet.create({
     main: {
+        display: 'flex',
         width: "100%",
         height: "100%",
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#FFF'
+        flexDirection: "column",
+        justifyContent: 'space-evenly',
+        backgroundColor: '#FFF',
+        alignItems: "center",
     },
     calendar: {
         aspectRatio: 1,
@@ -134,17 +136,19 @@ const styles = StyleSheet.create({
     },
     backgroundImageBox: {
         width: "100%",
-        height: "50%",
+        height: "30%",
         justifyContent: "center"
     },
     backgroundImage: {
-        resizeMode: "contain",
+        resizeMode: "stretch",
         width: "100%",
         height: "100%",
     },
     buttonBox: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
         width: "100%",
-        height: "50%",
         alignItems: "center"
     }
 })
