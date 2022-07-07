8f9eb5774984ffaf07ed3408ae718b795278de59
Octillerysnacker <octillerysnacker@gmail.com>
2022-07-07 18:40:17 -0500

    added scroll view

--- frontend/src/screens/adminScreen.tsx
+++ frontend/src/screens/adminScreen.tsx
@@ -1,119 +1,148 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, TouchableOpacity, ActivityIndicator } from "react-native";
-import FirebaseInteractor from "../firebase/firebaseInteractor";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ActivityIndicator,
+  ScrollView,
+} from "react-native";
+
 import PrimaryButton from "../components/Button/PrimaryButton";
-import { generateRoundsCSV, generateTestRoundsCSV, promptExportEmail } from "../firebase/csvExporter";
 import ErrorText from "../components/ErrorText";
+import {
+  generateRoundsCSV,
+  generateTestRoundsCSV,
+  promptExportEmail,
+} from "../firebase/csvExporter";
+import FirebaseInteractor from "../firebase/firebaseInteractor";
 
 const fi = new FirebaseInteractor();
 
 export interface AdminScreenProps {
-    route: any;
+  route: any;
 }
 
 export default function AdminScreen(props: AdminScreenProps) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorState, setError] = useState<string | undefined>(undefined);
 
-    const [isLoading, setIsLoading] = useState(false);
-    const [errorState, setError] = useState<string | undefined>(undefined)
-
-    async function tryExport(func: () => Promise<Object[]>) {
-        setIsLoading(true);
-        setError(undefined);
-        try {
-            await promptExportEmail(await func());
-        } catch (e: any) {
-            setError("There was a problem while exporting. There might be faulty data, or there is something wrong with your network connection. Error: " + e.toString());
-            throw e;
-        } finally {
-            setIsLoading(false);
-        }
+  async function tryExport(func: () => Promise<object[]>) {
+    setIsLoading(true);
+    setError(undefined);
+    try {
+      await promptExportEmail(await func());
+    } catch (e: any) {
+      setError(
+        "There was a problem while exporting. There might be faulty data, or there is something wrong with your network connection. Error: " +
+          e.toString()
+      );
+      throw e;
+    } finally {
+      setIsLoading(false);
     }
+  }
 
-    return (
-        <View style={styles.view}>
-            <Text>Press the buttons below to export data as an email with a CSV attachment.</Text>
+  return (
+    <ScrollView style={styles.view}>
+      <Text>
+        Press the buttons below to export data as an email with a CSV
+        attachment.
+      </Text>
 
-            <Text style={styles.header}>Practice Data</Text>
+      <Text style={styles.header}>Practice Data</Text>
 
-            <View style={styles.paragraph}>
-                <Text>The Practice Data CSV will contain the following information for all practice rounds played by participants:</Text>
-                <Text> • User ID</Text>
-                <Text> • Game Type</Text>
-                <Text> • Number of Turkish Words</Text>
-                <Text> • Session ID</Text>
-                <Text> • Round ID</Text>
-                <Text> • Start Time</Text>
-                <Text> • Duration</Text>
-                <Text> • Score</Text>
-            </View>
+      <View style={styles.paragraph}>
+        <Text>
+          The Practice Data CSV will contain the following information for all
+          practice rounds played by participants:
+        </Text>
+        <Text> • User ID</Text>
+        <Text> • Game Type</Text>
+        <Text> • Number of Turkish Words</Text>
+        <Text> • Session ID</Text>
+        <Text> • Round ID</Text>
+        <Text> • Start Time</Text>
+        <Text> • Duration</Text>
+        <Text> • Score</Text>
+      </View>
 
-            <PrimaryButton onPress={async () => {
-                await tryExport(generateRoundsCSV);
-            }} disabled={isLoading} text="Export Practice CSV" style={styles.button} />
+      <PrimaryButton
+        onPress={async () => {
+          await tryExport(generateRoundsCSV);
+        }}
+        disabled={isLoading}
+        text="Export Practice CSV"
+        style={styles.button}
+      />
 
-            <Text style={styles.header}>Round Data</Text>
+      <Text style={styles.header}>Round Data</Text>
 
-            <View style={styles.paragraph}>
-                <Text>The Test Data CSV will contain the following information for all test questions answered by participants:</Text>
-                <Text> • Participant ID</Text>
-                <Text> • Game Type</Text>
-                <Text> • Test Session ID</Text>
-                <Text> • Question Number</Text>
-                <Text> • Start Time</Text>
-                <Text> • End Time</Text>
-                <Text> • Duration</Text>
-                <Text> • Answer</Text>
-                <Text> • Correctness</Text>
-            </View>
+      <View style={styles.paragraph}>
+        <Text>
+          The Test Data CSV will contain the following information for all test
+          questions answered by participants:
+        </Text>
+        <Text> • Participant ID</Text>
+        <Text> • Game Type</Text>
+        <Text> • Test Session ID</Text>
+        <Text> • Question Number</Text>
+        <Text> • Start Time</Text>
+        <Text> • End Time</Text>
+        <Text> • Duration</Text>
+        <Text> • Answer</Text>
+        <Text> • Correctness</Text>
+      </View>
 
-            <PrimaryButton onPress={async () => {
-                await tryExport(generateTestRoundsCSV)
-            }} disabled={isLoading} text="Export Test CSV" style={styles.button} />
-            <View style={styles.error}>
-                <ErrorText message={errorState} />
-            </View>
-            {
-                isLoading &&
-                <View style={styles.loading}>
-                    <ActivityIndicator />
-                </View>
-            }
+      <PrimaryButton
+        onPress={async () => {
+          await tryExport(generateTestRoundsCSV);
+        }}
+        disabled={isLoading}
+        text="Export Test CSV"
+        style={styles.button}
+      />
+      <View style={styles.error}>
+        <ErrorText message={errorState} />
+      </View>
+      {isLoading && (
+        <View style={styles.loading}>
+          <ActivityIndicator />
         </View>
-    )
+      )}
+    </ScrollView>
+  );
 }
 
-
-const defaultStyle = StyleSheet.create({
-
-});
+const defaultStyle = StyleSheet.create({});
 
 const styles = StyleSheet.create({
-    view: {
-        padding: "5%"
-    },
-    button: {
-        width: "96%",
-        marginVertical: 10,
-        marginHorizontal: "2%"
-    },
-    header: {
-        fontWeight: "500",
-        fontSize: 20,
-        marginVertical: 10
-    },
-    paragraph: {
-        marginVertical: 10
-    },
-    error: {
-        marginTop: 30
-    },
-    loading: {
-        position: 'absolute',
-        width: '110%',
-        height: '110%',
-        zIndex: 10,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#00000099'
-    }
-})
+  view: {
+    padding: "5%",
+  },
+  button: {
+    width: "96%",
+    marginVertical: 10,
+    marginHorizontal: "2%",
+  },
+  header: {
+    fontWeight: "500",
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  paragraph: {
+    marginVertical: 10,
+  },
+  error: {
+    marginTop: 30,
+  },
+  loading: {
+    position: "absolute",
+    width: "110%",
+    height: "110%",
+    zIndex: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#00000099",
+  },
+});
