5ccb777bdfdf4bfe037130b366c1d6e133d8ec2b
Shashank Jarmale <shashankjarmale1@gmail.com>
2022-08-21 12:55:37 -0400

    Changed onboarding text on second screen for pairing vs selecting

--- frontend/src/screens/GameScreen/GameScreenFlow.tsx
+++ frontend/src/screens/GameScreen/GameScreenFlow.tsx
@@ -1,22 +1,24 @@
-import { useEffect, useState } from "react"
-import FirebaseInteractor from "../../firebase/firebaseInteractor"
+import { useEffect, useState } from "react";
+
+import FirebaseInteractor from "../../firebase/firebaseInteractor";
 import { GameScreenProps } from "./gameScreen";
 import PairingGameScreen from "./pairingGameScreen";
 import SelectingGameScreen from "./selectingGameScreen";
 
 const fi = new FirebaseInteractor();
-export default function GameScreenFlow(props : GameScreenProps) {
-  const [gameType, setGameType] = useState("pairing");
-  const fi = new FirebaseInteractor();
-  
-  useEffect(() => {
-    fi.getUser().then(user => {
-      setGameType(user.gameType);
-    })}, []);
 
-    if (gameType === "pairing") {
-      return PairingGameScreen(props);
-    } else {
-      return SelectingGameScreen(props);
-    }
+export default function GameScreenFlow(props: GameScreenProps) {
+  const [gameType, setGameType] = useState("pairing");
+
+  useEffect(() => {
+    fi.getUser().then((user) => {
+      setGameType(user.gameType);
+    });
+  }, []);
+
+  if (gameType === "pairing") {
+    return PairingGameScreen(props);
+  } else {
+    return SelectingGameScreen(props);
+  }
 }
--- frontend/src/screens/Onboarding/OnboardingScreens.tsx
+++ frontend/src/screens/Onboarding/OnboardingScreens.tsx
@@ -1,63 +1,92 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
-import OnboardingScreen from './OnboardingScreen';
-import { TURQUOISE, BLUE, PURPLE } from '../../constants/colors';
-import Swiper from 'react-native-swiper';
+import React, { useEffect, useState } from "react";
+import { StyleSheet } from "react-native";
+import Swiper from "react-native-swiper";
+
+import { TURQUOISE, BLUE, PURPLE } from "../../constants/colors";
+import FirebaseInteractor from "../../firebase/firebaseInteractor";
+import OnboardingScreen from "./OnboardingScreen";
+
+const fi = new FirebaseInteractor();
 
 interface OnboardingScreenProps {
-    navigation: any;
-    route: any;
+  navigation: any;
+  route: any;
 }
 
-export default function OnboardingScreens({ navigation, route }: OnboardingScreenProps) {
-    return (
-        <Swiper
-            showsButtons={false}
-            loop={false}
-            pagingEnabled={true}
-            dotStyle={styles.dots}
-            activeDotStyle={styles.activeDots}>
-            <OnboardingScreen
-                bgColor={BLUE}
-                screenContent={
-                    "Thank you for agreeing to \nparticipate in our study! " +
-                    "For the next week, your goal is to learn as many " +
-                    "Turkish words as possible."} />
-            <OnboardingScreen
-                bgColor={TURQUOISE}
-                screenContent={
-                    "To achieve your goal, you'll be using this app!\n\n " +
-                    "Here is how it works:\n • You'll see a set of English " +
-                    "words and a set of Turkish words.\n • You'll try to " +
-                    "match each English word to the correct Turkish word, " +
-                    "then you'll receive feedback.\n • The more you practice, " +
-                    "the better you'll get!"} />
-            <OnboardingScreen
-                bgColor={PURPLE}
-                screenContent={
-                    "Over the next week, you can use this app to practice " +
-                    "as much or as little as you like. At the end of the week, " +
-                    "you'll receive a vocabulary test. The better you do on the " +
-                    "test, the more money you'll win!"}
-                hasNavButton={true}
-                navButtonTitle={"done"}
-                navigation={navigation}
-                route={"HomeScreen"} />
-        </Swiper>
-    )
+export default function OnboardingScreens({
+  navigation,
+  route,
+}: OnboardingScreenProps) {
+  const [gameType, setGameType] = useState("pairing");
+
+  useEffect(() => {
+    fi.getUser().then((user) => {
+      setGameType(user.gameType);
+    });
+  }, []);
+
+  return (
+    <Swiper
+      showsButtons={false}
+      loop={false}
+      pagingEnabled
+      dotStyle={styles.dots}
+      activeDotStyle={styles.activeDots}
+    >
+      <OnboardingScreen
+        bgColor={BLUE}
+        screenContent={
+          "Thank you for agreeing to \nparticipate in our study! " +
+          "For the next week, your goal is to learn as many " +
+          "Turkish words as possible."
+        }
+      />
+      <OnboardingScreen
+        bgColor={TURQUOISE}
+        screenContent={
+          gameType === "pairing"
+            ? "To achieve your goal, you'll be using this app!\n\n " +
+              "Here is how it works:\n • You'll see a set of English " +
+              "words and a set of Turkish words.\n • You'll try to " +
+              "match each English word to the correct Turkish word, " +
+              "then you'll receive feedback.\n • The more you practice, " +
+              "the better you'll get!"
+            : "To achieve your goal, you'll be using this app!\n\n " +
+              "Here is how it works:\n • You'll see one English word " +
+              "and a set of Turkish words.\n • You'll try to match " +
+              "the English word to the correct Turkish word, then you'll " +
+              "receive feedback.\n • The more you practice, " +
+              "the better you'll get!"
+        }
+      />
+      <OnboardingScreen
+        bgColor={PURPLE}
+        screenContent={
+          "Over the next week, you can use this app to practice " +
+          "as much or as little as you like. At the end of the week, " +
+          "you'll receive a vocabulary test. The better you do on the " +
+          "test, the more money you'll win!"
+        }
+        hasNavButton
+        navButtonTitle="done"
+        navigation={navigation}
+        route="HomeScreen"
+      />
+    </Swiper>
+  );
 }
 
 const styles = StyleSheet.create({
-    dots: {
-        backgroundColor: 'rgba(255,255,255,0.5)',
-        width: 16,
-        height: 16,
-        borderRadius: 16
-    },
-    activeDots: {
-        backgroundColor: '#FFF',
-        width: 16,
-        height: 16,
-        borderRadius: 16
-    }
+  dots: {
+    backgroundColor: "rgba(255,255,255,0.5)",
+    width: 16,
+    height: 16,
+    borderRadius: 16,
+  },
+  activeDots: {
+    backgroundColor: "#FFF",
+    width: 16,
+    height: 16,
+    borderRadius: 16,
+  },
 });
